7a63669261649e8d5c45a72ebea6d98eadb017a5
Jon Loucks <j.loucks@comcast.net>
2026-02-16 09:04:55 -0500

    smoke test compile error

--- src/test/smoke/src/test/smoke.test.ts
+++ src/test/smoke/src/test/smoke.test.ts
@@ -6,6 +6,6 @@ test("variants-ts exports", () => {
   ok(typeof createInstaller === "function", "createInstaller should be a function.");
   ok(typeof VERSION === "string", "VERSION should be a string.");
   ok(BOOTSTRAPPED === true, "BOOTSTRAPPED should be true.");
-  ok(null as Installer === null, "Installer type should be defined.");
-  ok(null as InstallerConfig === null, "InstallerConfig type should be defined.");
+  ok(null as unknown as Installer === null, "Installer type should be defined.");
+  ok(null as unknown as InstallerConfig === null, "InstallerConfig type should be defined.");
 });
